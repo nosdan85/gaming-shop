5413d33f57183d53e79f8a2f365f0bccd5e13fe4
nosdan85 <susnguyensus@gmail.com>
2026-04-12 19:44:35 +0700

    Use Vercel route proxy for shop/admin APIs to avoid function limit

--- client/api/shop/orders.js
+++
@@ -1,5 +0,0 @@
-import { proxyToBackendPath } from '../_proxy.js';
-
-export default async function handler(req, res) {
-    return proxyToBackendPath(req, res, '/api/shop/orders');
-}
--- client/api/shop/paypal-email.js
+++
@@ -1,5 +0,0 @@
-import { proxyToBackendPath } from '../_proxy.js';
-
-export default async function handler(req, res) {
-    return proxyToBackendPath(req, res, '/api/shop/paypal-email');
-}
